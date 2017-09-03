82e02f895b67fa7c5d7cd2b6bcace66ec199e653
Ara <ayaghsizian@gmail.com>
2017-09-03 12:44:24 -0400

    alphavantage test api is working will look into it more

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 $('.divChange').text('lsdjfalsdjf');
 
-$.ajax({
-  
-});
+
+function testApi(){
+  $.ajax({
+    url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo",
+    success: function(data){
+      console.log(data);
+    }
+  });//ajax
+
+}//testApi
