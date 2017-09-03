9f6361b8ad7c29931182e57845eb7abb2574f17f
Ara <ayaghsizian@gmail.com>
2017-09-03 14:15:51 -0400

    testApiPrice function works need 2b more specific

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-
-
-apikey = "FJTIVZL1XTSG7F2E";
-alphaVantageUrl = "https://www.alphavantage.co/query?";
+var apikey = "FJTIVZL1XTSG7F2E";
+var alphaVantageUrl = "https://www.alphavantage.co/query?";
+var daily = "function=TIME_SERIES_DAILY";
 function testApi(){
+
   $.ajax({
-    url: alphaVantageUrl + "function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey="+apikey,
+    url: alphaVantageUrl + daily + "&symbol=MSFT&interval=1min&apikey=" + apikey,
     success: function(data){
       $('.divChange').text('Success');
-      $('.divChange').append(data[ 'Meta Data' ][ '2. Symbol' ]);
+      $('.divChange').append('<br>' + data[ 'Meta Data' ][ '2. Symbol' ]);
       console.log(data[ 'Meta Data' ][ '2. Symbol' ]);
     }
   });//ajax
 
 }//testApi
+
+function testApiPrice(){
+
+  $.ajax({
+    url: alphaVantageUrl + daily + "&symbol=MSFT&interval=1min&apikey=" + apikey,
+    success: function(data){
+
+      $('.divPrice').text('Success!');
+      $('.divChange').append('<br>' + data);
+      console.log(data);
+
+    }//success
+  });//ajax
+}//testApiPrice
