5ef2c3b2ef04988844b5eea7154d4f72a127775f
Ara <ayaghsizian@gmail.com>
2017-09-03 13:14:49 -0400

    got MSFT to show up on screen

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-$('.divChange').text('lsdjfalsdjf');
 
 
+apikey = "FJTIVZL1XTSG7F2E";
+alphaVantageUrl = "https://www.alphavantage.co/query?";
 function testApi(){
   $.ajax({
-    url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo",
+    url: alphaVantageUrl + "function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey="+apikey,
     success: function(data){
-      console.log(data);
+      $('.divChange').text('Success');
+      $('.divChange').append(data[ 'Meta Data' ][ '2. Symbol' ]);
+      console.log(data[ 'Meta Data' ][ '2. Symbol' ]);
     }
   });//ajax
 
